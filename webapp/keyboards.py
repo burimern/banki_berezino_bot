1db2dbd8db257bbfe51268a5a09f787cd2f70d10
burimern <burimskijnikita6@gmail.com>
2025-08-19 23:42:44 +0300

    Update keyboards.py

--- webapp/keyboards.py
+++ webapp/keyboards.py
@@ -1,1 +1,11 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
+def get_main_menu(webapp_url: str):
+    keyboard = InlineKeyboardMarkup(row_width=1)
+    keyboard.add(
+        InlineKeyboardButton(
+            text="🔥 Открыть магазин",
+            web_app=WebAppInfo(url=webapp_url)
+        )
+    )
+    return keyboard
